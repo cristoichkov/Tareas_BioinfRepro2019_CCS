7f7b13796166800d5aeeabce3a0f0539ae1f7abe
Cacto <cristoichkov@gmail.com>
2019-01-25 13:36:05 -0600

    Version de entrega

--- Mammillaria_TGCA.sh
+++ Mammillaria_TGCA.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
+## Este script baja 5 secuencias de Mammillaria de NCBI
+## Baja 5 secuencias en un archivo llamado Mamm.fasta
 curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&rettype=fasta&id=AY545239.1,AY545256.1,AY545268.1,AY545272.1,AY545323.1" > Mamm.fasta
-grep -oE ' \Mammillaria \w+ |TCGA' Mamm.fasta
+## Busca el genero Mammillaria y la especie, así como la secuencia 'TGCA' que contiene cada una
+grep -oE ' \Mammillaria \w+ |TGCA' Mamm.fasta
